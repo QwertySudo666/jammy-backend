e1c047e7772a85e4b7c0e8f2e4077de4efb3a3ca
darkbrother <krupskiy25.12.00@gmail.com>
2025-04-21 17:33:52 +0300

    added some files

---
+++ jammy-image-reader/src/main/java/com/jammy/jammyimagereader/JammyImageReaderApplication.java
@@ -0,0 +1,13 @@
+package com.jammy.jammyimagereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JammyImageReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JammyImageReaderApplication.class, args);
+	}
+
+}
---
+++ jammy-image-reader/src/test/java/com/jammy/jammyimagereader/JammyImageReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jammy.jammyimagereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JammyImageReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
